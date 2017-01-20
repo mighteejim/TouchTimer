0533ae8f020532e81cd2e196fd7242cfa9746190
Jim <jackley@fox.local>
2017-01-20 15:25:21 -0500

    Initial Commit

---
+++ TouchTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchTimer
+//
+//  Created by Jim on 1/20/17.
+//  Copyright © 2017 GormTech. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
